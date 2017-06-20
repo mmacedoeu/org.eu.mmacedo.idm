97a1f49f1234974cfe674395b088762e2beb1bb6
mmacedoeu <contato@mmacedo.eu.org>
2017-06-19 21:03:40 -0300

    include protocol on origin

--- src/main/java/org/eu/mmacedo/idm/security/CorsFilter.java
+++ src/main/java/org/eu/mmacedo/idm/security/CorsFilter.java
@@ -22,7 +22,7 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         final HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "www.crudglobo.ml");
+        response.setHeader("Access-Control-Allow-Origin", "https://www.crudglobo.ml");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE, PATCH");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
         response.setHeader("Access-Control-Max-Age", "3600");
